c034b5c61b9d513685cd364e24711ef61120b1aa
Nur Alfian Julianda <nalfianjuli@gmail.com>
2019-08-21 15:58:59 +0700

    add record audo rename view controller

--- PitchPerfect/RecordSoundViewController.swift
+++ PitchPerfect/RecordSoundViewController.swift
@@ -7,12 +7,15 @@
 //
 
 import UIKit
+import AVFoundation
 
-class ViewController: UIViewController {
+class RecordSoundViewController: UIViewController {
 
     @IBOutlet weak var recordLabel: UILabel!
     @IBOutlet weak var recordButton: UIButton!
     @IBOutlet weak var stopRecord: UIButton!
+    
+    var audioRecorder : AVAudioRecorder!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -33,6 +36,23 @@ class ViewController: UIViewController {
         recordLabel.text = "Record in Progress"
         recordButton.isEnabled = false
         stopRecord.isEnabled = true
+        recordNow()
+    }
+    
+    func recordNow() {
+        let dirPath = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)[0] as String
+        let recordName = "recordedVoice.wav"
+        let pathArray = [dirPath, recordName]
+        let filePath = URL(string: pathArray.joined(separator: ""))
+        
+        let session = AVAudioSession.sharedInstance()
+        try! session.setCategory(AVAudioSession.Category.playAndRecord, mode: AVAudioSession.Mode.default, options: AVAudioSession.CategoryOptions.defaultToSpeaker)
+        
+        try! audioRecorder = AVAudioRecorder(url: filePath!, settings: [:])
+        audioRecorder.isMeteringEnabled
+        audioRecorder.prepareToRecord()
+        audioRecorder.record()
+        
     }
     
     @IBAction func stopRecord(_ sender: Any) {
